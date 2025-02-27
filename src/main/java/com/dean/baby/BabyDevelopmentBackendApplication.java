cee77e56828fc4092e89aa6db5fa69c171dbed4b
Dean.Yang <i64991189@gs.ncku.edu.tw>
2025-02-27 14:40:22 +0800

    init

---
+++ src/main/java/com/dean/baby/BabyDevelopmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dean.baby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyDevelopmentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyDevelopmentBackendApplication.class, args);
+    }
+
+}
